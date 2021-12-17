900bec49d412544cd4d955844f7ac78925e6cb55
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2021-12-18 00:01:10 +0100

    added guess page

---
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,17 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, message: "", score: 0)}
+  end
+
+  @impl true
+  def handle_event("guess", %{"number" => number}, socket) do
+    {:ok, assign(socket, message: "Your guess: #{number}. Wrong. Guess again. ", score: socket.assigns.score - 1)}
+  end
+
+  def time() do
+    DateTime.utc_now() |> utc_now()
+  end
+end
--- pento/lib/pento_web/router.ex
+++ pento/lib/pento_web/router.ex
@@ -18,6 +18,7 @@ defmodule PentoWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/guess", WrongLive
   end
 
   # Other scopes may use custom stacks.
