e9914ecaa4e1f3dcc096d2b0bbc56cba980a584c
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2021-12-30 00:07:42 +0100

    added live image upload handler in live component

--- pento/lib/pento_web/live/product_live/form_component.ex
+++ pento/lib/pento_web/live/product_live/form_component.ex
@@ -10,7 +10,13 @@ defmodule PentoWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+       accept: ~w(.jpg .jpeg .png),
+       auto_upload: true,
+       max_entries: 1,
+       progress: &handle_progress/3
+     )}
   end
 
   @impl true
@@ -51,5 +57,37 @@ defmodule PentoWeb.ProductLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp handle_progress(:image, entry, socket) do
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      {
+        :noreply,
+        socket
+        |> put_flash(:info, "File #{entry.client_name} uploaded")
+        |> update_changeset(:image_upload, path)
+      }
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
+    socket
+    |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
+  end
+
+  defp upload_static_file(%{path: path} = test, socket) do
+    # Plug in your production image file persistence implementation here!
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    Routes.static_path(socket, "/images/#{Path.basename(dest)}")
   end
 end
