4d8d60fa78ad21a9a771e9666a6847a406177bf9
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2021-12-29 14:13:41 +0100

    added promo recipient schema

---
+++ pento/lib/pento/promo/recipient.ex
@@ -0,0 +1,16 @@
+defmodule Pento.Promo.Recipient do
+  defstruct [:first_name, :email]
+  @types %{first_name: :string, email: :string}
+
+  alias Pento.Promo.Recipient
+
+  import Ecto.Changeset
+
+
+  def changeset(%Recipient{} = user, attrs) do
+    {user, @types}
+    |> cast(attrs, Map.keys(@types))
+    |> validate_required(Map.keys(@types))
+    |> validate_format(:email, ~r/@/)
+  end
+end
