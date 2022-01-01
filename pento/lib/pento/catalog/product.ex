3caea3fdf79036fb3483717dff4d97aa1f57fbcb
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-01 01:21:51 +0100

    created association of rating in product

--- pento/lib/pento/catalog/product.ex
+++ pento/lib/pento/catalog/product.ex
@@ -2,12 +2,16 @@ defmodule Pento.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Pento.Survey.Rating
+
   schema "products" do
     field :description, :string
     field :name, :string
     field :sku, :integer
     field :unit_price, :float
     field :image_upload, :string
+
+    has_many :ratings, Rating
 
     timestamps()
   end
