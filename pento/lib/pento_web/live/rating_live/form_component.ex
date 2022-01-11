d3979753914745ceb7da7261acae522c2f134492
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-11 23:25:37 +0100

    added form component to create ratings

--- pento/lib/pento_web/live/rating_live/form_component.ex
+++ pento/lib/pento_web/live/rating_live/form_component.ex
@@ -1,3 +1,23 @@
 defmodule PentoWeb.RatingLive.FormComponent do
   use PentoWeb, :live_component
+
+  alias Pento.{Survey, Survey.Rating}
+
+  def update(assigns, socket) do
+    {
+      :ok,
+      socket
+      |> assign(assigns)
+      |> assign_rating()
+      |> assign_changeset()
+    }
+  end
+
+  defp assign_rating(%{assigns: %{user: user, product: product}} = socket) do
+    assign(socket, :rating, %Rating{user_id: user.id, product_id: product.id})
+  end
+
+  defp assign_changeset(%{assigns: %{rating: rating}} = socket) do
+    assign(socket, :changeset, Survey.change_rating(rating))
+  end
 end
