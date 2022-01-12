25bd989f99c0b1dad74847955f5b3d39e0b48aa1
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-12 02:02:33 +0100

    added validate and save rating functions

--- pento/lib/pento_web/live/rating_live/form_component.ex
+++ pento/lib/pento_web/live/rating_live/form_component.ex
@@ -13,6 +13,33 @@ defmodule PentoWeb.RatingLive.FormComponent do
     }
   end
 
+  def handle_event("save", %{"rating" => rating_params}, socket) do
+    {:noreply, save_rating(socket, rating_params)}
+  end
+
+  def handle_event("validate", %{"rating" => rating_params}, socket) do
+    {:noreply, validate_rating(socket, rating_params)}
+  end
+
+  defp validate_rating(socket, rating_params) do
+    changeset =
+      socket.assigns.rating
+      |> Survey.change_rating(rating_params)
+      |> Map.put(:action, :validate)
+
+      assign(socket, changeset: changeset)
+  end
+
+  defp save_rating(%{assigns: %{product_index: product_index, product: product}} = socket, rating_params) do
+    case Survey.create_rating(rating_params) do
+      {:ok, rating} ->
+        product = %{product | ratings: [rating]}
+        send(self(), {:rating_created, product, product_index})
+        socket
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
+  end
   defp assign_rating(%{assigns: %{user: user, product: product}} = socket) do
     assign(socket, :rating, %Rating{user_id: user.id, product_id: product.id})
   end
