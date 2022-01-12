d921edebcea53b7b1593b85865c910ce86b63c59
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-12 01:40:49 +0100

    added font awesome

--- pento/assets/webpack.config.js
+++ pento/assets/webpack.config.js
@@ -41,6 +41,17 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              esModule: false,  
+              outputPath: '../fonts'
+            }
+          }]
         }
       ]
     },
