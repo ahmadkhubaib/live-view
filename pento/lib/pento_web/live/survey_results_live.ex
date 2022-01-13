459f135dc17b0b6030c3cd85567b3e2fbe0bf0de
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-13 16:00:25 +0100

    added survey results component

--- pento/lib/pento_web/live/survey_results_live.ex
+++ pento/lib/pento_web/live/survey_results_live.ex
@@ -1,3 +1,19 @@
 defmodule PentoWeb.SurveyResultsLive do
   use PentoWeb, :live_component
+
+  alias Pento.Catalog
+
+  def update(assigns, socket) do
+    {
+      :ok,
+      socket
+      |> assign(assigns)
+      |> assign_products_with_avg_ratings()
+    }
+  end
+
+  defp assign_products_with_avg_ratings(socket) do
+    socket
+    |> assign(:products_with_avg_ratings, Catalog.products_with_average_ratings())
+  end
 end
