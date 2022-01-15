24062ff6201449ee9c3d3fb2f14c0f52a1a49114
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-16 00:51:31 +0100

    added contex for drawing

--- pento/mix.exs
+++ pento/mix.exs
@@ -48,7 +48,8 @@ defmodule Pento.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false}
+      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false},
+      {:contex, "~> 0.4.0"}
     ]
   end
 
