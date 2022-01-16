bd50378c86d138f05b59d2f9fe3fed16078cdc8f
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-16 11:57:46 +0100

    underlined obsolete param

--- pento/lib/pento_web/live/live_helpers.ex
+++ pento/lib/pento_web/live/live_helpers.ex
@@ -15,9 +15,9 @@ defmodule PentoWeb.LiveHelpers do
         product: @product,
         return_to: Routes.product_index_path(@socket, :index) %>
   """
-  def live_modal(socket, component, opts) do
+  def live_modal(_socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
-    live_component(socket, PentoWeb.ModalComponent, modal_opts)
+    live_component(PentoWeb.ModalComponent, modal_opts)
   end
 end
