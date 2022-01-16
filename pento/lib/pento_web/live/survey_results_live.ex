a47d70560f0ae35b8fa0f0aac663abda17d21984
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-16 11:58:13 +0100

    added bar chart in admin dashboard

--- pento/lib/pento_web/live/survey_results_live.ex
+++ pento/lib/pento_web/live/survey_results_live.ex
@@ -9,7 +9,56 @@ defmodule PentoWeb.SurveyResultsLive do
       socket
       |> assign(assigns)
       |> assign_products_with_avg_ratings()
+      |> assign_dataset()
+      |> assign_chart()
+      |> assign_svg_chart()
     }
+  end
+
+  defp assign_svg_chart(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:svg_chart, make_svg_chart(chart))
+  end
+
+  defp make_svg_chart(chart) do
+    Contex.Plot.new(500, 500, chart)
+    |> Contex.Plot.titles(title(), subtitle())
+    |> Contex.Plot.axis_labels(x_axis(), y_axis())
+    |> Contex.Plot.to_svg()
+  end
+
+  defp title do
+    "Product Ratings"
+  end
+
+  defp subtitle do
+    "average star ratings per product"
+  end
+
+  defp x_axis() do
+    "products"
+  end
+
+  defp y_axis() do
+    "stars"
+  end
+
+  defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket
+    |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
+  end
+
+  defp assign_dataset(%{assigns: %{products_with_avg_ratings: products_with_avg_ratings}} = socket) do
+    socket
+    |> assign(:dataset, make_bar_chart_dataset(products_with_avg_ratings))
+  end
+
+  defp make_bar_chart_dataset(dataset) do
+    Contex.Dataset.new(dataset)
   end
 
   defp assign_products_with_avg_ratings(socket) do
