7bd69b6c4342acb3dd42284dc8c0f870302b48b5
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-01-30 21:24:21 +0100

    added real time tracking with pubsub

--- pento/lib/pento_web/live/admin_dashboard_live.ex
+++ pento/lib/pento_web/live/admin_dashboard_live.ex
@@ -1,11 +1,25 @@
 defmodule PentoWeb.AdminDashboardLive do
   use PentoWeb, :live_view
 
+  alias PentoWeb.Endpoint
+  @survey_results_topic "survey_results"
+
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Endpoint.subscribe(@survey_results_topic)
+    end
     {
       :ok,
       socket
       |> assign(:survey_results_component_id, "survey-results")
     }
+  end
+
+  def handle_info(%{event: "rating_created"}, socket) do
+    send_update(
+      PentoWeb.SurveyResultsLive,
+      id: socket.assigns.survey_results_component_id
+    )
+    {:noreply, socket}
   end
 end
--- pento/lib/pento_web/live/survey_live.ex
+++ pento/lib/pento_web/live/survey_live.ex
@@ -2,6 +2,8 @@ defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
 
   alias Pento.{Survey, Accounts}
+  alias PentoWeb.Endpoint
+  @survey_results_topic "survey_results"
 
   @impl true
   def mount(_params, %{"user_token" => user_token} = _session, socket) do
@@ -25,6 +27,7 @@ defmodule PentoWeb.SurveyLive do
   end
 
   def handle_rating_created(%{assigns: %{products: products}} = socket, product, product_index) do
+    Endpoint.broadcast(@survey_results_topic, "rating_created", %{})
     socket
     |> put_flash(:info, "#{product.name} rating created")
     |> assign(:products, List.replace_at(products, product_index, product))
