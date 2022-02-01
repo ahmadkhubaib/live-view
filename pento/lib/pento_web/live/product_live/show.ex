87a6e5d3edff778624c3c4fda3757531a76fe0b4
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2022-02-01 10:33:04 +0100

    added presence for tracking users looking at show page

--- pento/lib/pento_web/live/product_live/show.ex
+++ pento/lib/pento_web/live/product_live/show.ex
@@ -2,19 +2,30 @@ defmodule PentoWeb.ProductLive.Show do
   use PentoWeb, :live_view
 
   alias Pento.Catalog
+  alias PentoWeb.Presence
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, %{"user_token" => user_token}, socket) do
+    {:ok, assign(socket, :user_token, user_token)}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    product = Catalog.get_product!(id)
+    maybe_track_user(product, socket)
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:product, Catalog.get_product!(id))}
+     |> assign(:product, product)}
   end
+
+  defp maybe_track_user(product, %{assigns: %{live_action: :show, user_token: user_token}} = socket) do
+    if connected?(socket) do
+      Presence.track_user(self(), product, socket.assigns.live_action, socket.assigns.user_token)
+    end
+  end
+
+  defp maybe_track_user(_product, _socket), do: nil
 
   defp page_title(:show), do: "Show Product"
   defp page_title(:edit), do: "Edit Product"
--- pento/lib/pento_web/presence.ex
+++ pento/lib/pento_web/presence.ex
@@ -1,3 +1,19 @@
 defmodule PentoWeb.Presence do
   use Phoenix.Presence, otp_app: :pento, pubsub_server: Pento.PubSub
+
+  alias PentoWeb.Presence
+  alias Pento.Accounts
+
+  @user_activity_topic "user_activity"
+
+  def track_user(pid, product, user_token) do
+    user = Accounts.get_user_by_session_token(user_token)
+
+    case Presence.get_by_key(@user_activity_topic, product.name) do
+      [] ->
+        Presence.track(pid, @user_activity_topic, product.name, %{users: [%{email: user.email}]})
+      %{users: active_users_for_product} ->
+        Presence.update(pid, @user_activity_topic, product.name, %{users: [active_users_for_product | %{email: user.email}]})
+    end
+  end
 end
